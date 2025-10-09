d987dac2ef8390b69eb745bd6612e61db077e24c
RafaelCosta <rafacosta92@hotmail.com>
2025-10-09 22:34:37 +0100

    Atualização

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,52 @@ initialCards.forEach((card) => {
   console.log(card.name);
 });
 
+
+
+const editProfileButton = document.querySelector('.profile__edit-button');
+const editPopup = document.querySelector('#edit-popup');
+const closeEditButton = editPopup.querySelector('.popup__close');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const formElement = editPopup.querySelector('.popup__form');
+const nameInput = editPopup.querySelector('.popup__input_type_name');
+const jobInput = editPopup.querySelector('.popup__input_type_description');
+
+
+function openModal(modal) {
+  modal.classList.add('popup_is-opened');
+}
+
+function closeModal(modal) {
+  modal.classList.remove('popup_is-opened');
+}
+
+
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+}
+
+
+closeEditButton.addEventListener('click', () => closeModal(editPopup));
+
+
+editProfileButton.addEventListener('click', handleOpenEditModal);
+
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closeModal(editPopup);
+}
+
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
